1b30bd462b80fdd350e80aa6b94b0d5143e43d11
WangLinXMusic <pyaesone5official@gmail.com>
2024-10-12 13:28:17 +0630

    Add files via upload

---
+++ WangLinXMusic/__init__.py
@@ -0,0 +1,30 @@
+from WangLinXMusic.core.bot import Hotty
+from WangLinXMusic.core.dir import dirr
+from WangLinXMusic.core.git import git
+from WangLinXMusic.core.userbot import Userbot
+from WangLinXMusic.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Hotty()
+userbot = Userbot()
+api = SafoneAPI()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+APP = "BRANDED_KUDI_BOT"  # connect music api key "Dont change it"
---
+++ WangLinXMusic/__main__.py
@@ -0,0 +1,62 @@
+import asyncio
+import importlib
+from sys import argv
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from WangLinXMusic import LOGGER, app, userbot
+from WangLinXMusic.core.call import Hotty
+from WangLinXMusic.misc import sudo
+from WangLinXMusic.plugins import ALL_MODULES
+from WangLinXMusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("WangLinXMusic.plugins" + all_module)
+    LOGGER("WangLinXMusic.plugins").info("Successfully Imported Modules...")
+    await userbot.start()
+    await Hotty.start()
+    try:
+        await Hotty.stream_call("https://graph.org/file/e999c40cb700e7c684b75.mp4")
+    except NoActiveGroupCall:
+        LOGGER("WangLinXMusic").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Hotty.decorators()
+    LOGGER("WangLinXMusic").info(
+        "ᴅʀᴏᴘ ʏᴏᴜʀ ɢɪʀʟꜰʀɪᴇɴᴅ'ꜱ ɴᴜᴍʙᴇʀ ᴀᴛ @BRANDED_PAID_CC ᴊᴏɪɴ @BRANDRD_BOT , @BRANDED_WORLD ꜰᴏʀ ᴀɴʏ ɪꜱꜱᴜᴇꜱ"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("WangLinXMusic").info("Stopping Brandrd Music Bot...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
